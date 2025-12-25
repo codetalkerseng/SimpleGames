3e744ec58e1fc288a7c06e9db674e1322d4ab360
Jordan <73709354+codetalkerseng@users.noreply.github.com>
2025-12-25 11:55:19 -0700

    Update gen-firebase-key-json.js

--- gen-firebase-key-json.js
+++ gen-firebase-key-json.js
@@ -7,10 +7,10 @@ import fs from 'fs';
 const credentials = {
   type: "service_account",
   project_id: "solitear-91ba6",
-  private_key_id: "[YOUR_PRIVATE_KEY_ID]", // TODO: Replace with your private_key_id from service account JSON
+  private_key_id: "28e4b1771772e71d59893fb3a26ccf7e0f1a3a79", // TODO: Replace with your private_key_id from service account JSON
   private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'), // This comes from AppFlow secret
-  client_email: "[YOUR_CLIENT_EMAIL]", // TODO: Replace with your client_email (e.g., firebase-adminsdk-xxxxx@solitear-91ba6.iam.gserviceaccount.com)
-  client_id: "[YOUR_CLIENT_ID]", // TODO: Replace with your client_id from service account JSON
+  client_email: "firebase-adminsdk-fbsvc@solitear-91ba6.iam.gserviceaccount.com", // TODO: Replace with your client_email (e.g., firebase-adminsdk-xxxxx@solitear-91ba6.iam.gserviceaccount.com)
+  client_id: "116409103134931765119", // TODO: Replace with your client_id from service account JSON
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
   token_uri: "https://oauth2.googleapis.com/token",
   auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
